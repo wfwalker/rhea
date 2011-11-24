e35e3a1cd5d741df66abe496fc189d0d64aeca84
Bill Walker <bwalker@mozilla.com>
2011-11-23 20:42:11 -0800

    nicer date formats; better table listing of KML's

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def short_nice_date(date)
+    h date.strftime("%b %d, %Y")
+  end          
+
 end
