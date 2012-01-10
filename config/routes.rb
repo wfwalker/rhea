58019b09d46b0641f009d3f84cddadc511a57231
Bill Walker <bwalker@mozilla.com>
2012-01-10 14:52:21 -0800

    add sensible root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :kmls
 
+  # wfw root URL
+  map.connect '', :controller => 'kmls', :action => 'index'
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
